852a169c33ee557bd078bf662df78110d4b85905
Oleg Gordeev <oleg.gordeev@sharp-dev.net>
2024-03-29 16:30:17 +0300

    Add class-transformer and validation pipe

--- apps/pw-application/src/main.ts
+++ apps/pw-application/src/main.ts
@@ -1,10 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const port = parseInt(process.env.API_PORT) || 3000;
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
 
   const config = new DocumentBuilder().build();
   const document = SwaggerModule.createDocument(app, config);
